8772321a65d440f5c613319dc73a3d34d3befc24
Usernamenigel <nigel.frangenberg@web.de>
2013-05-11 20:01:52 +0200

    menu

--- wba2_phase2/src/generated/Einleser.java
+++ wba2_phase2/src/generated/Einleser.java
@@ -15,63 +15,106 @@ import javax.xml.bind.Unmarshaller;
 
 public class Einleser {
 
-	
 	private static final String User_xml = "src/Testuser.xml";
 	private static final String Kanal_xml = "src/Testkanal.xml";
-	
+
+	static Scanner sc = new Scanner(System.in);
+
 	public static void main(String[] args) throws JAXBException, FileNotFoundException {
-		
+		int w = 0;
+
+		JAXBContext context = JAXBContext.newInstance("generated");
+		Unmarshaller um = context.createUnmarshaller();
+		Dienst user = (Dienst) um.unmarshal(new FileReader(User_xml));
+		Dienst kanal = (Dienst) um.unmarshal(new FileReader(Kanal_xml));
+
+		while (w == 0) {
+			System.out.println("1. Testdaten einfuügen");
+			System.out.println("2. Daten anzeigen");
+			w = sc.nextInt();
+
+			switch (w) {
+			case 1:
+				testdaten();
+				w = 0;
+				break;
+			case 2:
+				// Liste aller Benutzer
+				List<User> ulist = user.getUser();
+
+				// Liste aller Kanäle
+				List<Kanal> klist = kanal.getKanal();
+
+				// Gib die Benutzer aus User_xml
+				for (User u : ulist) {
+					System.out.println("Alter: " + u.getUalter() + "\t" + "Name:" + u.getUname());
+				}
+				// Kanal_xml
+				for (Kanal k : klist) {
+					System.out.println("Kanäle: ");
+					System.out.println(k.getKname());
+				}
+				w = 0;
+				break;
+			default:
+				System.out.println("Ungueltig");
+				w = 0;
+			}
+		}
+
+	}
+
+	
+	/** Falls Testkanal.xml oder Testuser.xml leer sein solle, hier Daten einfügen */
+	
+	public static void testdaten() throws JAXBException, FileNotFoundException {
+
 		System.out.println("HALLO");
-		
+
 		// Marshaller
 		JAXBContext context = JAXBContext.newInstance("generated");
 		Marshaller m = context.createMarshaller();
 		m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-		
+
 		// Unmarshaller: Holt Benutzer aus User_xml
 		Unmarshaller um = context.createUnmarshaller();
-	    Dienst user = (Dienst) um.unmarshal(new FileReader(User_xml));
-	    // Gleiche für Kanal_xml
-	    Dienst kanal = (Dienst) um.unmarshal(new FileReader(Kanal_xml));
-	    
+		Dienst user = (Dienst) um.unmarshal(new FileReader(User_xml));
+		// Gleiche für Kanal_xml
+		Dienst kanal = (Dienst) um.unmarshal(new FileReader(Kanal_xml));
 
-		
 		// Test Benutzer anlegen
 		User u1 = new User();
 		u1.setUalter(23);
 		u1.setUgeschlecht("m");
 		u1.setUid(1);
 		u1.setUname("Nigel");
-		
+
 		User u2 = new User();
 		u2.setUalter(5);
 		u2.setUgeschlecht("w");
 		u2.setUid(2);
 		u2.setUname("Test");
-		// Liste aller Benutzer
-		List<User> ulist = user.getUser();
-		
+
 		// Test Kanal anlegen
 		Kanal k1 = new Kanal();
 		k1.setKid(0);
 		k1.setKname("Meinurlaub");
 		k1.setKbetreiber(1);
-		// Liste aller Kanäle
-		List<Kanal> klist = kanal.getKanal();
-		
+
 		// Nigel wird nun in in die Userliste von Dienst geshrieben.
 		user.getUser().add(u1);
 		user.getUser().add(u2);
-		
+
 		kanal.getKanal().add(k1);
-		
+
 		// Die User List in Testuser.xml schreiben
 		m.marshal(user, new File(User_xml));
 		// Kanaäle in Testkanal.xml schreiben
 		m.marshal(kanal, new File(Kanal_xml));
-		
+
 		System.out.println("Fehlerfrei");
-		
 	}
+	
+	
 
 }
