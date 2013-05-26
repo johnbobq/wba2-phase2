299b86330468777793455c4dbe630464791ff3c8
Usernamenigel <nigel.frangenberg@web.de>
2013-05-26 20:40:41 +0200

    GET, DELETE, POST fuer User/Kanal

--- wba2_phase2/src/restservice/ChannelsService.java
+++ wba2_phase2/src/restservice/ChannelsService.java
@@ -1,19 +1,102 @@
 package restservice;
 
+import generated.Dienst;
+import generated.ObjectFactory;
+import generated.User;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 import javax.ws.rs.*;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+
+import generated.*;
 
 @Path( "/channels" )
 public class ChannelsService
 {
-   @GET @Produces( "text/plain" )
-   public String halloText( @QueryParam("name") String name )
-   {
-      return "Hier Liste der Kanäle ";
-   }
+   
+	@GET
+	@Produces("application/xml")
+	public Dienst getChannels() throws JAXBException, FileNotFoundException {
 
-   @GET @Produces( "text/html" )
-   public String halloHtml( @QueryParam("name") String name )
-   {
-      return "<html><title>HelloWorld</title><body><h2>Html: Hier Liste der Kanäle</h2></body></html>";
-   }
+		JAXBContext context = JAXBContext.newInstance("generated");
+		Unmarshaller um = context.createUnmarshaller();
+		Dienst channels = (Dienst) um.unmarshal(new FileReader("src/Testkanal.xml"));
+
+		return channels;
+	}
+	
+	@GET
+	@Path("/{Kid}")
+	@Produces("application/xml")
+	public Dienst getUser(@PathParam("Kid") int i) throws JAXBException, FileNotFoundException {
+
+		JAXBContext context = JAXBContext.newInstance("generated");
+		Unmarshaller um = context.createUnmarshaller();
+		Dienst channels = (Dienst) um.unmarshal(new FileReader("src/Testkanal.xml"));
+
+		ObjectFactory of = new ObjectFactory();
+		Dienst channel = of.createDienst();
+		channel.getKanal().add(channels.getKanal().get(i));
+		System.out.println("KANAL");
+		return channel;
+
+	}
+	
+	@DELETE
+	@Path("/{Kid}")
+	@Produces("application/xml")
+	public Dienst deleteUser(@PathParam("Kid") int i) throws JAXBException, FileNotFoundException {
+
+		JAXBContext context = JAXBContext.newInstance("generated");
+		Unmarshaller um = context.createUnmarshaller();
+		Dienst channels = (Dienst) um.unmarshal(new FileReader("src/Testkanal.xml"));
+
+		// users in user kopieren
+		ObjectFactory of = new ObjectFactory();
+		Dienst channel = of.createDienst();
+
+		// i-ten Benutzer aus user löschen
+		channel.getKanal().addAll(channels.getKanal());
+		channel.getKanal().remove(i);
+
+		// user "aktualisieren" und zurückgeben
+		// Marshaller
+		Marshaller m = context.createMarshaller();
+		m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+		m.marshal(channel, new File("src/Testkanal.xml"));
+
+		return channel;
+	}
+	
+	@POST
+	@Path("/{Kid}")
+	@Produces("application/xml")
+	public Dienst postUser(@FormParam("Kname") String name, @FormParam("Beschreibung") int beschreibung) throws JAXBException, FileNotFoundException {
+
+		
+		JAXBContext context = JAXBContext.newInstance("generated");
+		Unmarshaller um = context.createUnmarshaller();
+		Dienst channels = (Dienst) um.unmarshal(new FileReader("src/Testkanal.xml"));
+		
+
+		Kanal newKanal = new Kanal();
+		newKanal.setKname(name);
+		newKanal.setKid(channels.getKanal().size()+1);
+		
+		channels.getKanal().add(newKanal);
+
+		Marshaller m = context.createMarshaller();
+		m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+		m.marshal(channels, new File("src/Testkanal.xml"));
+		
+		return channels;
+
+	}
+	
 }
--- wba2_phase2/src/restservice/UserService.java
+++ wba2_phase2/src/restservice/UserService.java
@@ -1,19 +1,103 @@
 package restservice;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+import generated.*;
+
 import javax.ws.rs.*;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
 
-@Path( "/user" )
-public class UserService
-{
-   @GET @Produces( "text/plain" )
-   public String halloText( @QueryParam("name") String name )
-   {
-      return "Hier Liste der Benutzer ";
-   }
+@Path("/user")
+public class UserService {
 
-   @GET @Produces( "text/html" )
-   public String halloHtml( @QueryParam("name") String name )
-   {
-      return "<html><title>HelloWorld</title><body><h2>Html: Hier Liste der Benutzer</h2></body></html>";
-   }
+	/**
+	 * Wegen der Hierachie Dienst->User ist der Rückgabewert vom Typ Dienst, da
+	 * dieser die User enthält
+	 */
+	@GET
+	@Produces("application/xml")
+	public Dienst getUsers() throws JAXBException, FileNotFoundException {
+
+		JAXBContext context = JAXBContext.newInstance("generated");
+		Unmarshaller um = context.createUnmarshaller();
+		Dienst users = (Dienst) um.unmarshal(new FileReader("src/Testuser.xml"));
+
+		return users;
+	}
+
+	@GET
+	@Path("/{Uid}")
+	@Produces("application/xml")
+	public Dienst getUser(@PathParam("Uid") int i) throws JAXBException, FileNotFoundException {
+
+		JAXBContext context = JAXBContext.newInstance("generated");
+		Unmarshaller um = context.createUnmarshaller();
+		Dienst users = (Dienst) um.unmarshal(new FileReader("src/Testuser.xml"));
+
+		ObjectFactory of = new ObjectFactory();
+		Dienst user = of.createDienst();
+		user.getUser().add(users.getUser().get(i));
+		System.out.println("asas");
+		return user;
+
+	}
+
+	@DELETE
+	@Path("/{Uid}")
+	@Produces("application/xml")
+	public Dienst deleteUser(@PathParam("Uid") int i) throws JAXBException, FileNotFoundException {
+
+		JAXBContext context = JAXBContext.newInstance("generated");
+		Unmarshaller um = context.createUnmarshaller();
+		Dienst users = (Dienst) um.unmarshal(new FileReader("src/Testuser.xml"));
+
+		// users in user kopieren
+		ObjectFactory of = new ObjectFactory();
+		Dienst user = of.createDienst();
+
+		// i-ten Benutzer aus user löschen
+		user.getUser().addAll(users.getUser());
+		user.getUser().remove(i);
+
+		// user "aktualisieren" und zurückgeben
+		// Marshaller
+		Marshaller m = context.createMarshaller();
+		m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+		m.marshal(user, new File("src/Testuser.xml"));
+
+		return user;
+	}
+
+	/** Neuen Benutzer anlegen mit Name und Alter, ID wird berechnet durch länge der Nutzerliste + 1 */
+	@POST
+	@Path("/{Uid}")
+	@Produces("application/xml")
+	public Dienst postUser(@FormParam("Uname") String Name, @FormParam("Ualter") int Alter) throws JAXBException, FileNotFoundException {
+
+		
+		JAXBContext context = JAXBContext.newInstance("generated");
+		Unmarshaller um = context.createUnmarshaller();
+		Dienst users = (Dienst) um.unmarshal(new FileReader("src/Testuser.xml"));
+		
+
+		User newUser = new User();
+		newUser.setUname(Name);
+		newUser.setUalter(Alter);
+		newUser.setUid(users.getUser().size()+1);
+		
+		users.getUser().add(newUser);
+
+		Marshaller m = context.createMarshaller();
+		m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+		m.marshal(users, new File("src/Testuser.xml"));
+		
+		return users;
+
+	}
+
 }
