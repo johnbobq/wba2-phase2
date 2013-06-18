de795a79126bd678b2c3f304d13b703e6935891a
johnbobq <jq@in2web-design.de>
2013-06-18 10:56:12 +0200

    Update

---
+++ wba2_phase2/src/gui/Usergui.java
@@ -0,0 +1,113 @@
+package gui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JTabbedPane;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import java.awt.Color;
+import javax.swing.UIManager;
+import javax.swing.JLabel;
+import javax.swing.JComboBox;
+import javax.swing.JTextField;
+import javax.swing.JFormattedTextField;
+import org.eclipse.wb.swing.FocusTraversalOnArray;
+import java.awt.Component;
+
+public class Usergui {
+
+	private JFrame frame;
+	private JTextField textField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Usergui window = new Usergui();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Usergui() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 630, 520);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.setBackground(new Color(238, 238, 238));
+		tabbedPane.setBounds(6, 25, 620, 435);
+		frame.getContentPane().add(tabbedPane);
+		
+		JPanel panel_1 = new JPanel();
+		tabbedPane.addTab("Neuer Beitrag", null, panel_1, null);
+		panel_1.setBackground(UIManager.getColor("Button.background"));
+		panel_1.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Schreibe einen neuen Beitrag \u00FCber deinen urlaub!");
+		lblNewLabel.setBounds(6, 6, 355, 16);
+		panel_1.add(lblNewLabel);
+		
+		JLabel lblUrlaub = new JLabel("Urlaub");
+		lblUrlaub.setBounds(6, 34, 61, 16);
+		panel_1.add(lblUrlaub);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setBounds(50, 30, 155, 27);
+		panel_1.add(comboBox);
+		
+		textField = new JTextField();
+		textField.setToolTipText("");
+		textField.setBounds(211, 28, 134, 28);
+		panel_1.add(textField);
+		textField.setColumns(10);
+		
+		JFormattedTextField formattedTextField = new JFormattedTextField();
+		formattedTextField.setBounds(6, 62, 587, 233);
+		panel_1.add(formattedTextField);
+		
+		JButton btnSpeichern = new JButton("Speichern");
+		btnSpeichern.setBounds(6, 295, 117, 29);
+		panel_1.add(btnSpeichern);
+		panel_1.setFocusTraversalPolicy(new FocusTraversalOnArray(new Component[]{lblNewLabel, lblUrlaub, comboBox, textField, formattedTextField, btnSpeichern}));
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(238, 238, 238));
+		tabbedPane.addTab("Urlaubs Kan\u00E4le", null, panel, null);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel_1 = new JLabel("\u00DCbersicht aller Urlaubs Kan\u00E4le");
+		lblNewLabel_1.setBounds(6, 6, 248, 16);
+		panel.add(lblNewLabel_1);
+		
+		JLabel lblKanal = new JLabel("Kanal 1");
+		lblKanal.setBounds(6, 34, 223, 16);
+		panel.add(lblKanal);
+		
+		JButton btnAnzeigen = new JButton("Anzeigen");
+		btnAnzeigen.setBounds(241, 29, 117, 29);
+		panel.add(btnAnzeigen);
+		
+		JLabel lblUrlaub_1 = new JLabel("Urlaub");
+		lblUrlaub_1.setBounds(6, 6, 61, 16);
+		frame.getContentPane().add(lblUrlaub_1);
+	}
+}
